bd7a79a3f92758badd5e22ebde270ba056416cfc
ritik2727 <ritikjain2727@gmail.com>
2023-09-06 12:15:45 +0530

    db setup in backend

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import ConnectDB from "./backend/config/db.js";
+import colors from "colors";
 
+dotenv.config();
+ConnectDB();
 
+const app = express();
 
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `server running ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+      .yellow.bold
+  )
+);
