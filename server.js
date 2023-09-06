5a098398ad29f417202bd6765911b34182c69f54
ritik2727 <ritikjain2727@gmail.com>
2023-09-06 14:06:26 +0530

    code deploy to vercel

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ import dotenv from "dotenv";
 import ConnectDB from "./backend/config/db.js";
 import colors from "colors";
 import userRoutes from "./backend/routes/userRoutes.js";
-
+import path from "path";
 import  cors from 'cors'; 
-
+   
 
 
 dotenv.config();
@@ -15,11 +15,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
-
 app.use("/api/users",userRoutes);
+
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} 
+else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
+
+
 
 const PORT = process.env.PORT || 5000;
 
